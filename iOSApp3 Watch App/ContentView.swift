783928c094a01b18afc58f41679f54876e242311
KennethPlumstead <Kennethplumstead@icloud.com>
2025-10-26 19:35:05 -0400

    Initial Commit

---
+++ iOSApp3 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp3 Watch App
+//
+//  Created by Kenneth Plumstead on 2025-10-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp3 Watch App/iOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp3App.swift
+//  iOSApp3 Watch App
+//
+//  Created by Kenneth Plumstead on 2025-10-26.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
